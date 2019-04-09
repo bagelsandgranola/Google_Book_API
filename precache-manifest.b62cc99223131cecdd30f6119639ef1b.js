9cee82568cd55092638b39c02eeb844211c1aec1
Kyleigh <kysmith13@gmail.com>
2019-04-09 16:14:01 -0700

    Updates

---
+++ precache-manifest.b62cc99223131cecdd30f6119639ef1b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75cf3ad516e6ac9d1292",
+    "url": "/bagelsandgranola/Google_Book_API/static/css/main.6bda4d26.chunk.css"
+  },
+  {
+    "revision": "75cf3ad516e6ac9d1292",
+    "url": "/bagelsandgranola/Google_Book_API/static/js/main.3e0e0b67.chunk.js"
+  },
+  {
+    "revision": "746e78d6df60e413a6c7",
+    "url": "/bagelsandgranola/Google_Book_API/static/js/runtime~main.a0931bc0.js"
+  },
+  {
+    "revision": "7c1e8cde0f1f57d059dd",
+    "url": "/bagelsandgranola/Google_Book_API/static/js/2.7e675ef3.chunk.js"
+  },
+  {
+    "revision": "235a917af01b9f3a06c78ad7d4785e26",
+    "url": "/bagelsandgranola/Google_Book_API/index.html"
+  }
+];
